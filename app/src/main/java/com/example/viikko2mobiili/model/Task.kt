962b06969b8faf4e60751cbc273cca1688d27a98
JeremiasGitit <jeremias.katajisto@gmail.com>
2026-01-30 20:22:21 +0200

    viikko3valmis

--- app/src/main/java/com/example/viikko2mobiili/model/Task.kt
+++ app/src/main/java/com/example/viikko2mobiili/model/Task.kt
@@ -1,4 +1,4 @@
-package com.example.viikko2mobiili.domain
+package com.example.viikko2mobiili.model
 
 data class Task(
     val id: Int,
--- app/src/main/java/com/example/viikko2mobiili/model/TaskFunctions.kt
+++ app/src/main/java/com/example/viikko2mobiili/model/TaskFunctions.kt
@@ -1,4 +1,4 @@
-package com.example.viikko2mobiili.domain
+package com.example.viikko2mobiili.model
 
 fun addTask(list: List<Task>, newTask: Task): List<Task> {
     return list + newTask
